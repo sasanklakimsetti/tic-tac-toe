5ab2722b47c7a202b30c2dd36a9afdb2598f0249
sasanklakimsetti <saisasank.lakimsetti@gmail.com>
2024-08-11 21:59:54 +0530

    updated files

---
+++ src/components/resetButton.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./resetButton.css"
+export const ResetButton=({resetBoard})=>{
+    return(
+        <button className="reset-btn"onClick={resetBoard}>Reset</button>
+    )
+}
